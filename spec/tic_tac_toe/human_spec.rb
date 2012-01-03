ad28366cbf098568910f2b29edbd56fb97622ec6
Scott Reis <scott.reis@vertive.com>
2012-01-03 10:43:45 -0600

    remove tests that dont need filling in and fix play again checking

--- spec/tic_tac_toe/human_spec.rb
+++ spec/tic_tac_toe/human_spec.rb
@@ -5,19 +5,6 @@ module TicTacToe
 
     let(:human) { Human.new }
 
-    describe '#move' do
-      context 'given a valid move' do
-        it 'executes the move' do
-          pending #TODO
-        end
-      end
-      context 'given an invalid move' do
-        it 'asks for a valid move' do
-          pending #TODO
-        end
-      end
-    end
-
     describe '#mark' do
       it 'should return X' do
         human.mark.should === 'X'
